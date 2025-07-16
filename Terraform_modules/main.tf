483602b0f4699615a208a1ec797b093f9f13fb0c
luci <gfuonxner@gmail.com>
2025-07-16 16:11:12 -0500

    added something.tfvars

--- Terraform_modules/main.tf
+++ Terraform_modules/main.tf
@@ -1,9 +1,9 @@
 
-resource 'aws_s3_bucket' 'rotation_bucket' {
+resource "aws_s3_bucket" "rotation_bucket" {
   bucket  =var.bucket_name
   force_destroy = true
 }
 /*creates an S3 bucket with the name bucket_name
 and then allows terraform to destroy the bucket even if it has files in it*/
 
-
+#
---
+++ Terraform_modules/something.tfvars
@@ -0,0 +1,1 @@
+bucket_name = "some-kind-of-value"
