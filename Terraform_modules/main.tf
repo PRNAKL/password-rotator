654903c2ee0d7312e06c0d9f11828201e6575ee7
luci <gfuonxner@gmail.com>
2025-07-16 20:10:03 -0500

    added a new resource block

--- Terraform_modules/main.tf
+++ Terraform_modules/main.tf
@@ -6,4 +6,15 @@ resource "aws_s3_bucket" "rotation_bucket" {
 /*creates an S3 bucket with the name bucket_name
 and then allows terraform to destroy the bucket even if it has files in it*/
 
-#
+resource "aws_lambda_function" "rotation_lambda" {
+  function_name = var.function_name
+  role = var.role_arn
+  handler = var.handler
+  runtime = var.runtime
+  filename = var.lambda_zip
+   source_code_hash = filebase64sha256(var.lambda_zip)
+}
+
+environment {
+  variables = var.environment_vars
+}
