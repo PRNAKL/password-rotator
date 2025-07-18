60f623179224271090f01eab133d1df930dc8ae3
Gunner Fox <126932099+Luci5274@users.noreply.github.com>
2025-07-17 22:51:03 -0500

    Delete Terraform_modules/s3 directory

--- Terraform_modules/s3/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "rotation_bucket" {
-  bucket        = var.bucket_name
-  force_destroy = true  # Allows deletion even if the bucket has objects
-}
--- Terraform_modules/s3/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "bucket_name" {
-  description = "Globally unique name for the S3 bucket"
-  type        = string
-}
