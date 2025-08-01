c20314072c812db316fa49c05014db60398cef29
luci <gfuonxner@gmail.com>
2025-08-01 15:10:30 -0500

    made a small improvement to the index counting

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-#  Conditionally Create S3 Bucket
+# Conditionally Create S3 Bucket
 resource "aws_s3_bucket" "my_bucket" {
   count         = var.existing_bucket_name == "" ? 1 : 0
   bucket        = "prnakl-terraform-bucket-${random_id.bucket_suffix.hex}"
@@ -17,6 +17,7 @@ resource "aws_s3_bucket" "my_bucket" {
 
 # Unified Bucket Reference
 locals {
-  bucket_name = var.existing_bucket_name != "" ? var.existing_bucket_name : aws_s3_bucket.my_bucket[0].bucket
+  bucket_name = var.existing_bucket_name != "" ? var.existing_bucket_name :
+    (length(aws_s3_bucket.my_bucket) > 0 ? aws_s3_bucket.my_bucket[0].bucket : "")
   bucket_arn  = "arn:aws:s3:::${local.bucket_name}"
 }
