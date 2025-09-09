0cb2c7f15164ef512c3a574f2a9a53e83c7f66ca
luci <gfuonxner@gmail.com>
2025-09-09 13:19:44 -0500

    ok maybe this?

--- iam.tf
+++ iam.tf
@@ -155,3 +155,62 @@ resource "aws_scheduler_schedule" "every_24_hrs" {
     role_arn = aws_iam_role.scheduler_role.arn
   }
 }
+
+# IAM Policy: Full SNS + CloudWatch + Logs
+resource "aws_iam_policy" "full_cloudwatch_sns_policy" {
+  name        = "full_cloudwatch_sns_policy"
+  description = "Full SNS and CloudWatch access for Lambda and Terraform"
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "sns:*",
+          "logs:*",
+          "cloudwatch:*"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the policy to the Lambda execution role
+resource "aws_iam_role_policy_attachment" "attach_full_cloudwatch_sns" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.full_cloudwatch_sns_policy.arn
+}
+
+# SNS Topic for Lambda alerts
+resource "aws_sns_topic" "lambda_alerts" {
+  name = "lambda-error-alerts"
+}
+
+# SNS Email Subscription
+resource "aws_sns_topic_subscription" "email_alert" {
+  topic_arn = aws_sns_topic.lambda_alerts.arn
+  protocol  = "email"
+  endpoint  = "gunner.fox5274@gmail.com"
+}
+
+# CloudWatch Alarm for Lambda errors
+resource "aws_cloudwatch_metric_alarm" "lambda_error_alarm" {
+  alarm_name          = "LambdaErrorAlarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  statistic           = "Sum"
+  period              = 60
+  threshold           = 1
+  treat_missing_data  = "missing"
+
+  dimensions = {
+    FunctionName = "password_rotator"
+  }
+
+  alarm_description = "Alarm when Lambda errors occur"
+  alarm_actions     = [aws_sns_topic.lambda_alerts.arn]
+}
